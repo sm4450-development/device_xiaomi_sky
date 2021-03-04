26d60eb44c95e942a4ba7aad6443eba5cb693eee
suresh <suresh@codeaurora.org>
2021-03-04 12:46:03 -0800

    Revert "Enable RPMB override flag for listener"
    This reverts commit 2bdd216e5a5352691767f4eefcc3b6e1eb200b47.
    
    Change-Id: I4fcaa17c3311b0557e97ab5731276f416969b152

--- taro.mk
+++ taro.mk
@@ -68,7 +68,7 @@ TARGET_USES_QMAA_RECOMMENDED_BOOT_CONFIG := false
 #QMAA tech team flag to override global QMAA per tech team
 #true means overriding global QMAA for this tech area
 #false means using global, no override
-TARGET_USES_QMAA_OVERRIDE_RPMB := true
+TARGET_USES_QMAA_OVERRIDE_RPMB := false
 TARGET_USES_QMAA_OVERRIDE_DISPLAY := true
 TARGET_USES_QMAA_OVERRIDE_AUDIO   := false
 TARGET_USES_QMAA_OVERRIDE_VIDEO   := false
