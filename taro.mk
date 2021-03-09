c3f8ed6b1fd21331a02d4bc1466b9878d11f3527
Roopesh Nataraja <roopeshr@codeaurora.org>
2021-03-09 13:55:19 -0800

    taro: Remove deprecated HAL modules
    Remove below deprecated HAL modules.
      - android.hardware.configstore
      - android.hardware.contexthub
    
    Change-Id: I690e7b4a1cb38a39b699cd12831b9683e2435840

--- taro.mk
+++ taro.mk
@@ -193,8 +193,6 @@ PRODUCT_DEVICE := taro
 PRODUCT_BRAND := qti
 PRODUCT_MODEL := Taro for arm64
 
-PRODUCT_PACKAGES += android.hardware.configstore@1.1-service
-
 #----------------------------------------------------------------------
 # wlan specific
 #----------------------------------------------------------------------
@@ -322,11 +320,6 @@ PRODUCT_PACKAGES += libqrtr
 
 # diag-router
 TARGET_HAS_DIAG_ROUTER := true
-
-# Context hub HAL
-PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0-impl.generic \
-    android.hardware.contexthub@1.0-service
 
 # f2fs utilities
 PRODUCT_PACKAGES += \
