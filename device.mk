729ec5510ec6d31d40b802b6f7ad6bef805913bb
Fenglin Wu <quic_fenglinw@quicinc.com>
2022-06-27 23:12:24 +0800

    sky: healthd-ext: Add health HAL AIDL implementation
    Add health HAL AIDL implementation which is a service running for
    both health HAL and charger mode.
    
    Change-Id: I1f3205d1e34d93ed1739d5fa29c95a8f2b2d2894

--- device.mk
+++ device.mk
@@ -235,9 +235,10 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-impl-qti.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@1.0.vendor \
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
