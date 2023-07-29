c36e6dda29aa1e66c8ffae6212a266806d5b928e
Danny Baumann <dannybaumann@web.de>
2023-07-30 04:16:23 +0530

    sky: overlay: Enable option for full screen aspect ratio Change-Id: c31a9501567b4b8be0c6c4416bcd955e63ff22b3
    Signed-off-by: frozenking <frozenking1240196@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay-lineage
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
