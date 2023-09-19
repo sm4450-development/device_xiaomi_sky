47d8340ee23f1ec7f8d609e7cc370d1626eb8f57
Lostark13 <tushar9915@gmail.com>
2023-09-19 14:51:21 +0530

    sky: initial sepolicy
    Kanged from los marble

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,6 +209,10 @@ VENDOR_SECURITY_PATCH := 2023-08-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Vendor Boot
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
