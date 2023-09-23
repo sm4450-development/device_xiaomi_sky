173c312389167a0cbd5c9a02858c07d7622cc082
Arian <arian.kulmer@web.de>
2023-09-23 08:19:08 +0000

    sky: Add power HAL extension to handle dt2w

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,6 +183,9 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_BOARD_PLATFORM := parrot
 BOARD_USES_QCOM_HARDWARE := true
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
