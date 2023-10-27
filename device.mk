3e764d9c0d7f69b790aaaa3ca69b0841cca5d980
redducc <sushmit718@gmail.com>
2023-10-27 19:07:28 +0200

    sky: Build libstdc++.vendor from source

--- device.mk
+++ device.mk
@@ -379,6 +379,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
 
