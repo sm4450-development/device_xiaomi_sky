e76cd8fe043771ddf1d1a2af9a0aab311d3a0b0e
redducc <sushmit718@gmail.com>
2023-12-18 00:12:30 -0500

    sky: Build libstdc++ from source

--- device.mk
+++ device.mk
@@ -491,6 +491,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libstdc++_vendor
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
 
