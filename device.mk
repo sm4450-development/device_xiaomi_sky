20bec2e17f9c84e7c41aeb6c30b5a816e8b5e457
redducc <sushmit718@gmail.com>
2023-12-18 01:31:09 -0500

    sky: Import and update perf configs from LA.QSSI.14.0.r1

--- device.mk
+++ device.mk
@@ -335,6 +335,11 @@ PRODUCT_BOOT_JARS += \
     UxPerformance
 
 # Power
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/perf,$(TARGET_COPY_OUT_VENDOR)/etc/perf) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/lm,$(TARGET_COPY_OUT_VENDOR)/etc/lm) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/pwr,$(TARGET_COPY_OUT_VENDOR)/etc/pwr)
+
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
