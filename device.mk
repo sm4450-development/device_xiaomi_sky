f143609fe1aea8e74cebe2c91a352bb5259d451b
redducc <sushmit718@gmail.com>
2023-12-18 01:21:42 -0500

    sky: Update perf stack from CLO LA.QSSI.14.0.r1

--- device.mk
+++ device.mk
@@ -323,7 +323,16 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.3.vendor
+    libpsi.vendor \
+    libtflite
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.3.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
 
 # Power
 PRODUCT_PACKAGES += \
@@ -450,6 +459,7 @@ PRODUCT_COPY_FILES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti-v2 \
+    android.hardware.thermal@2.0 \
     android.hardware.thermal@2.0.vendor
 
 PRODUCT_VENDOR_PROPERTIES += \
