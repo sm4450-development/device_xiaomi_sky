fff92e8ae3eb0982ce0ef54b02ec3056da18dbf1
Omkar Chandorkar <gotenksIN@aospa.co>
2023-12-29 13:04:07 +0900

    sky: switch to common Health AIDL
    Change-Id: I41b10ed01f27cb8adff537c9ea4dfe72ccf4e280
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -200,8 +200,10 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-sky \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.xiaomi \
+    android.hardware.health-service.xiaomi_recovery \
+    android.hardware.health@1.0.vendor \
+    android.hardware.health@2.1.vendor
 
 # Hidl
 PRODUCT_PACKAGES += \
