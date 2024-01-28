2b741aa9869d24a43ae5edb4196f94def17cea81
Anay Wadhera <anay1018@gmail.com>
2024-01-28 12:54:42 +0000

    sky: Patch keymint to include rsk aidl for missing symbol
    * Also build some necessary packages
    
    Change-Id: Iad47fb49883eb3e6af3726815e2f996c35aaab09

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,9 @@ function blob_fixup() {
 	vendor/lib64/libhme.so)
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
+        vendor/lib64/libqtikeymint.so)
+            "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
+            ;;
     esac
 }
 
