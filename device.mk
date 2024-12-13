4efede8d049d16f313461b1fc1af0ffcae11c7a1
IPSBHANGU <inderpreetbhangu2@gmail.com>
2024-12-13 06:43:06 +0000

    sky: Build libusbhost to vendor
    Change-Id: I5cd223bc9952aca217332da114696dd6e458a2da

--- device.mk
+++ device.mk
@@ -405,6 +405,10 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.usb.config=mtp,adb
 endif
 
+# Usb
+PRODUCT_PACKAGES += \
+    libusbhost.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
