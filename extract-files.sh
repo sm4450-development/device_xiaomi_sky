11e8c53e618fa96a7b39932cb28cabe73e2c757c
Arian <arian.kulmer@web.de>
2024-12-13 06:38:37 +0000

    sky: Allow setsockopt syscall for qcom c2audio ``` $ strace -f vendor/bin/hw/vendor.qti.media.c2audio@1.0-service [...] [pid  5194] socket(AF_UNIX, SOCK_SEQPACKET|SOCK_CLOEXEC, 0) = 13 [pid  5194] setsockopt(13, SOL_SOCKET, SO_SNDTIMEO, "\5\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 16) = 13 [pid  5194] --- SIGSYS {si_signo=SIGSYS, si_code=SYS_SECCOMP, si_call_addr=0x7a905a365c, si_syscall=__NR_setsockopt, si_arch=AUDIT_ARCH_AARCH64} --- ```
    Change-Id: Ifa0420883cbf663f8c7576798b1f35031f660f54

--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,11 @@ function blob_fixup() {
             [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
+        vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy)
+            [ "$2" = "" ] && return 0
+            [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
+            grep -q "setsockopt: 1" "${2}" || echo "setsockopt: 1" >> "${2}"
+            ;;
     esac
 }
 
